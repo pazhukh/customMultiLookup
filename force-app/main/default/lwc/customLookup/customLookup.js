4316ce4c5c396c1acaaac75e0f5c8f6ca10abb2e
pazhukh <pahukh@gmail.com>
2021-02-04 15:59:14 +0200

    custom lookup init

---
+++ force-app/main/default/lwc/customLookup/customLookup.js
@@ -0,0 +1,84 @@
+import { LightningElement, wire, api, track } from 'lwc';
+import lookUp from '@salesforce/apex/LWCCalendarController.search';
+
+export default class CustomLookup extends LightningElement {
+    @api objName = 'Opportunity';
+    @api fieldsName = 'Id, Name';
+    @api whereClauseField = 'Name';
+    @api iconName = 'standard:opportunity';
+    @api filter = '';
+    @api alwaysShowLookup = false;
+
+    selectedName;
+    records;
+    isValueSelected;
+    blurTimeout;
+
+    searchTerm;
+    boxClass = 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-has-focus';
+    inputClass = '';
+
+    @wire(lookUp, {searchTerm : '$searchTerm', myObject : '$objName', fields: '$fieldsName', whereClauseField: '$whereClauseField', filter : '$filter'})
+    wiredRecords({error, data}) {
+        if (data) {
+            this.error = undefined;
+            this.records = data;
+        } else if (error) {
+            this.error = error;
+            this.records = undefined;
+        }
+    }
+
+    handleClick() {
+        this.searchTerm = '';
+        this.inputClass = 'slds-has-focus';
+        this.boxClass = 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-has-focus slds-is-open';
+    }
+
+    onBlur() {
+        this.blurTimeout = setTimeout(() => {
+            this.boxClass = 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-has-focus'
+        }, 200);
+    }
+
+    onSelect(event) {
+        let selectedRecord = this.records.filter(record => record.Id === event.currentTarget.dataset.id)[0];
+        console.log('[onSelect] selectedRecord', selectedRecord);
+
+        let eventDetail = {
+            selectedRecord: selectedRecord,
+            objName: this.objName
+        }
+        console.log('[onSelect] eventDetail', eventDetail);
+
+        this.fireEvent(eventDetail);
+
+        this.isValueSelected = !this.alwaysShowLookup;
+        this.selectedName = selectedRecord.Name;
+        if (this.blurTimeout) {
+            clearTimeout(this.blurTimeout);
+        }
+        this.boxClass = 'slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-has-focus';
+        console.log('[CustomLookup] [onSelect]', this.selectedName);
+    }
+
+    handleRemovePill() {
+        let eventDetail = {
+            objName: this.objName
+        }
+        this.fireEvent(eventDetail);
+        this.isValueSelected = false;
+    }
+
+    onChange(event) {
+        this.searchTerm = event.target.value;
+    }
+
+    fireEvent(detail) {
+        const valueSelectedEvent = new CustomEvent('lookupselected', {
+            detail:  detail
+        });
+        this.dispatchEvent(valueSelectedEvent);
+    }
+
+}
